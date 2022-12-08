3ac2657f893401603f4a06b7984edf85b593f0be
Mateusz Wołowczyk <matewol324@student.polsl.pl>
2022-12-08 12:37:03 +0100

    MDAVT-25 Added __init__ method and methods declarations for specific files extensions parsing

--- src/dataSource.py
+++ src/dataSource.py
@@ -1,7 +1,28 @@
 from data import Data
+import pathlib
 
 
 class DataSource:
 
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file_extension = pathlib.Path(file_name).suffix
+
+    def __parse_csv(self):
+        print('.csv')
+
+    def __parse_xlsx(self):
+        print('.xlsx')
+
+    def __parse_txt(self):
+        print('.txt')
+
     def parse_data(self):
-        return Data
+        if self.file_extension == '.csv':
+            self.__parse_csv()
+        elif self.file_extension == '.xlsx':
+            self.__parse_xlsx()
+        elif self.file_extension == '.txt':
+            self.__parse_txt()
+        else:
+            print('Unsupported file extension')
