5002cd08d6909d1ae768a2680360b5bf62ecd925
daniliw382 <liwinski.d@gmail.com>
2022-12-08 12:13:26 +0100

    MDAVT-28 Basic test UI implementation

--- src/app.py
+++ src/app.py
@@ -1,4 +1,30 @@
-class App:
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import sys
 
-    def execute(self):
-        pass
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super(QMainWindow, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("MDAVT")
+
+        label = QLabel("Test string")
+        label.setAlignment(Qt.AlignCenter)
+
+        self.setCentralWidget(label)
+
+
+def window():
+    app = QApplication(sys.argv)
+    window = App()
+    window.show()
+    app.exec_()
+
+
+window()
